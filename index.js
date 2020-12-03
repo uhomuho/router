bc847387d9a6f8e4e4b8c6850ccc7efc401a9428
uhomuho <uhomuho@gmail.com>
2020-12-03 23:45:56 +0500

    Commit

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const express = require('express'),
 			axios = require('axios'),
 			app = express(),
 			proxy = require('express-http-proxy'),
+			cors = require('cors')
 			url = 'http://localhost:8081'
+
+app.use(cors({
+  origin : "http://localhost:8083",
+  credentials: true,
+}))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use('/', proxy('http://tomcat.utyf.ru/api', { secure: false }));
+app.use('/', proxy('http://tomcat.utyf.ru/api'));
 
 let PORT = process.env.PORT || 8083
 const server = app.listen( PORT, () => {
